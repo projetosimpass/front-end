384731615bf72eb7ceb35ecb56537059ea769095
MatheusAraujo110 <theusnascimento386@gmail.com>
2024-09-19 23:30:49 -0300

    criando pastas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { RecuperacaoComponent } from './pages/recuperacao/recuperacao.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'registro',
+        component: RegistroComponent,
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'perfil',
+        component: PerfilComponent,
+    },
+    {
+        path: 'recuperacao',
+        component: RecuperacaoComponent,
+    }
+];
---
+++ src/app/pages/recuperacao/recuperacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './recuperacao.component.html',
+  styleUrl: './recuperacao.component.scss'
+})
+export class RecuperacaoComponent {
+
+}
